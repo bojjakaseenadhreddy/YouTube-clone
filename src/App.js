f1e3d91b27e57275ec5c6518698e1eb6ea9df3dd
kaseenadh Reddy Bojja <kaseenadh111@gmail.com>
2024-06-08 07:50:50 -0500

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>YouTube Clone APP</h1>
+      <h1 className='text-3xl'>YouTube Clone APP</h1>
     </div>
   );
 }
